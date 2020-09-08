bd04f431056d6512278fc21ac55a7294f2fdcfef
pb211 <57361718+pb211@users.noreply.github.com>
2020-09-08 13:50:18 +0800

    Update RO53_II_MissingNumbersOf0ToNLess1.java

--- RO53_II_MissingNumbersOf0ToNLess1.java
+++ RO53_II_MissingNumbersOf0ToNLess1.java
@@ -1,20 +1,21 @@
 package find;
 public class RO53_II_MissingNumbersOf0ToNLess1 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] nums={0,1,3};
 		int res= missingNumber(nums);
 		System.out.println(res);
-		//System.out.println(nums.length);
 	}
 	public static int missingNumber(int[] nums){
-		int a=0;
-		for(int i=0;i<nums.length;i++){
-			
-			if(nums[i]!=i){
-				return i;
+		 if(nums.length<=1){
+	            if(nums[0]==0){return 1;}
+	            if(nums[0]==1){return 0;}
+	        }
+			for(int i=0;i<nums.length;i++){
+				if(nums[i]!=i){
+					return i;
+				}
+	          
 			}
-		}
-		return a;
-	}
+			return nums.length;
+	    }
 }
