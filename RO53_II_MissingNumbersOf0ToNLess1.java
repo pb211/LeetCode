1b7f0a797d344dd8bcdd16bcefee572e0acf0e78
pb211 <57361718+pb211@users.noreply.github.com>
2020-09-08 14:15:56 +0800

    Update RO53_II_MissingNumbersOf0ToNLess1.java

--- RO53_II_MissingNumbersOf0ToNLess1.java
+++ RO53_II_MissingNumbersOf0ToNLess1.java
@@ -5,7 +5,16 @@ public class RO53_II_MissingNumbersOf0ToNLess1 {
 		int res= missingNumber(nums);
 		System.out.println(res);
 	}
-	public static int missingNumber(int[] nums){
+	public static int missingNumber(int[] nums) {
+		int i = 0, j = nums.length - 1;
+		while(i <= j) {
+		    int m = (i + j) / 2;
+		    if(nums[m] == m) i = m + 1;
+		    else j = m - 1;
+		}
+		return i;
+   	 }
+	public static int missingNumber1(int[] nums){
 		 if(nums.length<=1){
 	            if(nums[0]==0){return 1;}
 	            if(nums[0]==1){return 0;}
