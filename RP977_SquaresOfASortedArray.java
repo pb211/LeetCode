2bd143a318944110fb6f0b308cd062b20ad2adc8
pb211 <57361718+pb211@users.noreply.github.com>
2020-10-20 14:14:53 +0800

    Add files via upload

---
+++ RP977_SquaresOfASortedArray.java
@@ -0,0 +1,39 @@
+package Lecode.Array;
+
+import java.util.Arrays;
+
+public class RP977_SquaresOfASortedArray {
+    public static void main(String[] args) {
+        int A[] = {-4,-1,0,3,10};
+        int []res = sortedSquares(A);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] sortedSquares1(int[] A) {
+        int []res = new int[A.length];
+        for(int i=0;i<A.length;i++){
+            res[i]=A[i]*A[i];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+    public static int[] sortedSquares(int[] A) {
+        int[] res = new int[A.length];
+        int left=0;
+        int right=A.length-1;
+        int index=A.length-1;
+        while(index>=0){
+            int AL=A[left]*A[left];
+            int AR=A[right]*A[right];
+            if(AL>AR){
+                res[index]=AL;
+                left++;
+                index--;
+            }else{
+                res[index]=AR;
+                right--;
+                index--;
+            }
+        }
+        return res;
+    }
+}
